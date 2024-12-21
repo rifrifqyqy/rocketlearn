2b63e109bcde831e330f05963a997fcf1cb44519
rifrifqyqy <rifqyh22@gmail.com>
2024-12-21 09:10:46 +0700

    launch: create from crates

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+   String::from("Hello, world!")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
